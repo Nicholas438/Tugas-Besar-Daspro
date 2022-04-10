63a28ec124e69fad4d4098d665e6a377f17847e9
Nicholas <69570302+Nicholas438@users.noreply.github.com>
2022-04-10 22:04:38 +0700

    Update Souce Code.py

--- Souce Code.py
+++ Souce Code.py
@@ -62,7 +62,7 @@ def valid_input(number,type1,type2):
 # Inisialisasi mengubah semua csv menjadi list
 def parsing_file1(file,id,var1,var2,var3,var4,var5):
     # Subprogram untuk menginisialisasi file user.csv dan game.csv
-
+    global row_user,row_game
     file_user = open(file,'r')
     Lines = file_user.readlines()
     
@@ -110,12 +110,20 @@ def parsing_file1(file,id,var1,var2,var3,var4,var5):
                 break
         i+=1
         var5[count] =  ""
-        while i<(length(line)-2):
-            if line[i] !=";":
-                var5[count]+=line[i]
-                i+=1
-            else:
-                break
+        if (file == "user.csv" and count != (row_user-1)) or (file == "game.csv" and count != (row_game-1)):
+            while i<(length(line)-1):
+                if line[i] !=";":
+                    var5[count]+=line[i]
+                    i+=1
+                else:
+                    break
+        else:
+            while i<(length(line)):
+                if line[i] !=";":
+                    var5[count]+=line[i]
+                    i+=1
+                else:
+                    break
         count+=1
     return id,var1,var2,var3,var4,var5
 
@@ -161,12 +169,20 @@ def parsing_file2(id_game_riwayat,var1,var2,var3,var4):
                 break
         i+=1
         var4[count] = ""
-        while i<(length(line)-2):
-            if line[i] !=";":
-                var4[count]+=line[i]
-                i+=1
-            else:
-                break
+        if count != (row_riwayat-1):
+            while i<(length(line)-1):
+                if line[i] !=";":
+                    var4[count]+=line[i]
+                    i+=1
+                else:
+                    break
+        else:
+            while i<length(line):
+                if line[i] !=";":
+                    var4[count]+=line[i]
+                    i+=1
+                else:
+                    break            
         count+=1
     return id_game_riwayat,var1,var2,var3,var4
 
@@ -188,12 +204,20 @@ def parsing_file3(id_game_kepemilikan,var1):
                 break
         i+=1
         var1[count] = ""
-        while i<(length(line)-2):
-            if line[i] !=";":
-                var1[count]+=line[i]
-                i+=1
-            else:
-                break
+        if count != (row_kepemilikan-1):
+            while i<(length(line)-1):
+                if line[i] !=";":
+                    var1[count]+=line[i]
+                    i+=1
+                else:
+                    break
+        else:
+            while i<(length(line)):
+                if line[i] !=";":
+                    var1[count]+=line[i]
+                    i+=1
+                else:
+                    break
         count+=1
     return id_game_kepemilikan,var1
 
@@ -281,6 +305,15 @@ def tambah_game():
                 valid_input(harga_baru,"Harga","harga")
                 stok_baru = input("Masukkan stok awal: ")
                 valid_input(stok_baru,"Stok awal","stok awal")
+            nomor_game = ""
+            if (row_game%1000) <10:
+                nomor_game += "00"+str(row_game%1000)
+            elif (row_game%1000)<100:
+                nomor_game += "0" + str(row_game%1000)
+            else:
+                nomor_game += str(row_game%1000)
+            id_game_baru = "GAME" + nomor_game
+            id_game += [id_game_baru]
             nama_game += [nama_game_baru]
             kategori_game += [kategori_game]
             tahun_rilis += [tahun_rilis_baru]
@@ -334,17 +367,19 @@ def ubah_game():
 
 # F06. Mengubah stok game
 def ubah_stok():
-    global login_status,role,login_count,stok,id_game
+    global login_status,role,login_count,stok,id_game,nama_game
     if login_status == True:
         if role[login_count] == "admin":
             stok_id_game = input("Masukkan id game: ")
             stok_ubah = input("Masukkan jumlah: ")
+            valid_input(stok_ubah,"Stok","jumlah")
             id_game_found = False
             for i in range(row_game):
                 if stok_id_game == id_game[i]:
                     id_game_found = True
-                    if (stok[i] - stok_ubah)>=0:
-                        stok[i] -= stok_ubah
+                    if (int(stok[i]) + int(stok_ubah))>= 0:
+                        stok[i] = int(stok[i]) + int(stok_ubah)
+                        print("Selamat, stok game",nama_game[i],"sudah diupdate.")
                     else:
                         print("Stok game tidak bisa negatif")
             if id_game_found == False:
@@ -354,8 +389,107 @@ def ubah_stok():
     else:
         print("Fitur perlu akses login")
 
+# F07. Listing game
+def sort_ascending(obj,obj2,nama_game_sort,id_game_sort,kategori_game_sort,stok_sort):
+    global row_game
+    for j in range(1,row_game):
+        count_min = j
+        for i in range(j+1,row_game):
+            if obj[i]<obj[count_min]:
+                count_min = i 
+        if count_min != j:
+            obj[j],obj[count_min] = obj[count_min],obj[j]
+            nama_game_sort[j],nama_game_sort[count_min] = nama_game_sort[count_min],nama_game_sort[j]
+            id_game_sort[j],id_game_sort[count_min] = id_game_sort[count_min],id_game_sort[j]
+            obj2[j],obj2[count_min] = obj2[count_min],obj2[j]
+            kategori_game_sort[j],kategori_game_sort[count_min] = kategori_game_sort[count_min],kategori_game_sort[j]
+            stok_sort[j],stok_sort[count_min] = stok_sort[count_min],stok_sort[j]
+    return obj,nama_game_sort,id_game_sort,obj2,kategori_game_sort,stok_sort 
 
+def sort_descending(obj1,obj2,nama_game_sort,id_game_sort,kategori_game_sort,stok_sort):
+    global row_game
+    for j in range(1,row_game):
+        count_max = j
+        for i in range(j+1,row_game):
+            if obj1[count_max]<obj1[i]:
+                count_max = i 
+        obj1[j],obj1[count_max] = obj1[count_max],obj1[j]
+        nama_game_sort[j],nama_game_sort[count_max] = nama_game_sort[count_max],nama_game_sort[j]
+        id_game_sort[j],id_game_sort[count_max] = id_game_sort[count_max],id_game_sort[j]
+        obj2[j],obj2[count_max] = obj2[count_max],obj2[j]
+        kategori_game_sort[j],kategori_game_sort[count_max] = kategori_game_sort[count_max],kategori_game_sort[j]
+        stok_sort[j],stok_sort[count_max] = stok_sort[count_max],stok_sort[j]
+    return obj1,nama_game_sort,id_game_sort,obj2,kategori_game_sort,stok_sort   
 
+def sorting(skema,nama_game_sort,tahun_rilis_sort,id_game_sort,harga_sort,kategori_game_sort,stok_sort):
+    global tahun_rilis,nama_game,id_game,harga,kategori_game,stok
+    if skema == "tahun+":
+        sort_ascending(nama_game_sort,harga_sort,tahun_rilis_sort,id_game_sort,kategori_game_sort,stok_sort)
+    elif skema == "tahun-":
+        sort_descending(nama_game_sort,harga_sort,tahun_rilis_sort,id_game_sort,kategori_game_sort,stok_sort)
+    elif skema == "harga+":
+        sort_ascending(harga_sort,tahun_rilis_sort,nama_game_sort,id_game_sort,kategori_game_sort,stok_sort)
+    elif skema == "harga-":
+        sort_descending(harga_sort,tahun_rilis_sort,nama_game_sort,id_game_sort,kategori_game_sort,stok_sort)
+def list_game_toko():
+    global id_game, nama_game, harga,tahun_rilis, kategori_game,stok,login_status,row_game
+    if login_status == True:
+        print("Mengurutkan game di toko secara harga atau tahun rilis. Contoh skema: tahun+")
+        skema_sorting = input("Skema sorting: ")
+        tahun_rilis_sort = tahun_rilis
+        nama_game_sort = nama_game
+        id_game_sort = id_game
+        harga_sort = harga
+        kategori_game_sort = kategori_game
+        stok_sort = stok
+        sorting(skema_sorting,tahun_rilis_sort,nama_game_sort,id_game_sort,harga_sort,kategori_game_sort,stok_sort)
+        for i in range(1,row_game):
+            print(str(i)+"."+ id_game_sort[i]+" | "+ nama_game_sort[i] + " | "+harga_sort[i] + " | " + kategori_game_sort[i] + " | " + tahun_rilis_sort[i] + " | " + stok_sort[i])
+    else:
+        print("Fitur ini memerlukan akses login.")
+
+# F08. Membeli game
+def buy_game():
+    global id_game,id_game_kepemilikan,id_user,user_id_kepemilikan,row_game,row_kepemilikan,login_count,login_status,role,saldo,stok,nama_game,id_game_riwayat,nama_game_riwayat,user_id_riwayat,harga_riwayat,tahun_beli
+    id_game_beli = input("Masukkan ID Game: ")
+    if login_status == True:
+        if role[login_count] == "user":
+            for i in range(row_game):
+                found = False
+                if id_game[i] == id_game_beli:
+                    found = True
+                    harga_game_beli = harga[i]
+                    nama_game_beli = nama_game[i]
+                    game_index = i
+                    break
+            if found == True:
+                bought = False
+                for i in range(row_kepemilikan):
+                    if user_id_kepemilikan[i] == role[login_count] and id_game_kepemilikan[i] == id_game_beli:
+                        bought = True
+                        break
+                if bought == True:
+                    print("Anda sudah membeli game ini")
+                else:
+                    if int(saldo[login_count]) - int(harga_game_beli) <0:
+                        print("Saldo anda tidak cukup untuk membeli game tersebut!")
+                    else:
+                        saldo[login_count] = int(saldo[login_count]) - int(harga_game_beli)
+                        stok[game_index] = int(stok[game_index]) - 1
+                        user_id_kepemilikan += [id_user[login_count]]
+                        id_game_kepemilikan += [id_game_beli]
+                        id_game_riwayat += [id_game_beli]
+                        harga_riwayat += [harga_game_beli]
+                        user_id_riwayat += [id_user[login_count]]
+                        nama_game_riwayat += [nama_game_beli]
+                        tahun_beli += [2022] # Diganti per tahun 
+                        print("Selamat.",nama_game_beli,"berhasil dibeli!")
+            else:
+                print("Game tidak ditemukan")
+        else:
+            print("Admin tidak bisa membeli game")
+    else:
+        print("Fitur ini memerlukan akses login")
 
 # Inisialisasi parsing file user.csv
 row_user = count_row(file_user)
@@ -407,5 +541,8 @@ while True:
         ubah_game()
     elif perintah.lower() == "ubah_stok":
         ubah_stok()
-        
+    elif perintah.lower() == "list_game_toko":
+        list_game_toko()
+    elif perintah.lower() == "buy_game":
+        buy_game()
     perintah = input("Apa yang ingin kamu lakukan hari ini? Ketik help untuk melihat semua perintah yang ada \n")
